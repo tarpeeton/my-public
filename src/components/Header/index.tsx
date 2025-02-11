4fe1e917b846c6f177238439d7f5316fd7d56b8d
Yadgarov <yadgarovfayzullo0@gmail.com>
2025-02-11 23:25:31 +0500

    re-styled Header and changed Main/index

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,25 @@
+import { useTranslations } from "next-intl";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import "./header.css";
 import Image from "next/image";
 
 export const Header = () => {
+  const t = useTranslations("Header");
+
   return (
-    <div className="relative max-w-[95%] mx-auto bg-gradient-to-b from-[#4FADF7] to-[#0052D4] text-white p-10 rounded-3xl shadow-lg overflow-hidden">
+    <div className="relative max-w-[100%] mx-auto bg-gradient-to-b from-[#4FADF7] to-[#0052D4] text-white p-10 rounded-3xl shadow-lg overflow-hidden">
       <div className="max-w-5xl mx-auto flex flex-col md:flex-row items-center gap-6">
         {/* Текстовая часть */}
         <div className="flex-1">
-          <Card className="bg-white  rounded-xl shadow-lg">
+          <Card className="bg-white rounded-xl shadow-lg">
             <CardContent>
               <h1 className="text-3xl md:text-4xl font-bold text-black">
-                Найдите лучших врачей и клиники рядом с вами
+                {t("title")}
               </h1>
-              <p className="mt-2 text-lg text-gray-700">
-                Med Yordam — это современная платформа, которая объединяет
-                врачей, клиники и пациентов.
-              </p>
+              <p className="mt-2 text-lg text-gray-700">{t("description")}</p>
               <Button className="mt-4 bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 text-lg">
-                Найти врача
+                {t("button")}
               </Button>
             </CardContent>
           </Card>
@@ -27,10 +27,12 @@ export const Header = () => {
 
         {/* Графическая часть */}
         <div className="flex justify-center relative bottom-10">
-          <Image
-            src="./big-logo1.png"
+          <Image //НАДО ДОКОНЧИТЬ АНИМАЦИЮ И ПОДОБРАТЬ W/H
+            src="/big-logo1.png"
             alt="big-logo"
-            className="mb-6 animate-logo"
+            width={300}
+            height={500}
+            className="object-contain logo-anim"
           />
           <p className="absolute bottom-12 text-white text-4xl font-semibold">
             Med Yordam
@@ -39,8 +41,14 @@ export const Header = () => {
       </div>
 
       {/* Линия ЭКГ */}
-      <div className="absolute bottom-0 left-0 right-0">
-        <Image src="./Vector1.png" alt="" />
+      <div className=" bottom-0 left-0 right-0 relative">
+        <Image
+          src="/Vector1.png"
+          alt="big-logo"
+          width={2500}
+          height={2500}
+          className="object-contain"
+        />
       </div>
     </div>
   );
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Header from '../Header';
+import Filter from '../Doctor/Filter';
+import Advantages from "../Advantages/index"
+import FAQ from '@/components/Faq/Faq';
+import TopDoctos from "@/components/TopDoctors/TopDoctors"
 
 const Main = () => {
     return (
         <div>
-            
+            <Header />
+            <Filter />
+            <TopDoctos />
+            <Advantages />
+            <FAQ />
+
         </div>
     );
 };
