94ce71d106bd0b408e5c507243575cc26ae9d329
Yadgarov <yadgarovfayzullo0@gmail.com>
2025-02-18 15:54:53 +0500

    fixed error in unused imports

--- src/components/Clinics/Clinics.tsx
+++ src/components/Clinics/Clinics.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import { Card, CardContent } from "@/components/ui/card";
+import { Card} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import dynamic from "next/dynamic";
 import { FaPhone } from "react-icons/fa6";
-import { HiArrowRight } from "react-icons/hi";
 
 const YandexMap = dynamic(() => import("@/components/YandexMap/Map"), {
   ssr: false,
